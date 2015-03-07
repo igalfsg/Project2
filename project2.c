0364036c659784525c26a3ca5cad57545beb980c
Igal Flegmann Sandler <iflegman@ecegrid-thin1.ecn.purdue.edu>
2015-03-07 18:24:09 -0500

    more testing done

--- project2.c
+++ project2.c
@@ -16,16 +16,15 @@ void printArray(int * array, int size){
   int i;
   printf("[ ");
   for (i = 0; i < size; i++)
-    printf("%d ", array[i]);
+    printf("%d \n", array[i]);
   printf("]\n");
 }
- 
+ int cmpfunc (const void * a, const void * b)
+{
+   return ( *(int*)a - *(int*)b );
+}
  
 inline void radixSort(int * array, int size){
-  
-  printf("\n\nRunning Radix Sort on Unsorted List!\n\n");
- 
-  // Base 10 is used
   int i;
   int semiSorted[size];
   int significantDigit = 1;
@@ -36,20 +35,21 @@ inline void radixSort(int * array, int size){
   int tempo;
   int neglargest = 0;
   for(i = 0; i < size; i++){
-    if(array[i] < array[i - 1])
+    if(i >= 1 && array[i] < array[i - 1])
       wrongs++;
     else if(array[i] > largestNum)
       largestNum = array[i];
   }
+  printf("wrongs %d \n",wrongs);
   if (wrongs > (size - wrongs))
     {
-      for( i = 0; i < size; i++)
+      for( i = 0; i < (size / 2); i++)
 	{
 	  tempo = array[i];
 	  array[i] = array[size - i -1];
 	  array[size - i -1] = tempo;
 	}
-      wrongs = size - wrongs;
+      wrongs = size - wrongs - 1;
     }
   if (wrongs > 100)  
     {
@@ -143,26 +143,46 @@ int main(){
   
   printf("\n\nRunning Radix Sort Example in C!\n");
   printf("----------------------------------\n");
-  int size =10;
-  int list[10]={0, -2, 4, 5, 3, -4, 5, 6, 3, 2};
+  int size =1000000;
+  int list[1000000];
+  int check[size];
   int i;
   clock_t start, end;
   double cpu_timeu;
 
-  /* for(i = 0; i< size; i++) */
-  /*   { */
-  /*     list[i] = rand(); */
-  /*   } */
+  for(i = 0; i< size; i++)
+    {
+      list[i] = size;
+      check[i] = list[i];
+    }
+
+  for (i = 25; i < 200; i++)
+    {
+      list[i] = rand();
+      check[i] = list[i];
+    }
   
-  printf("\nUnsorted List: ");
-  printArray(&list[0], size);
+  //printf("\nUnsorted List: ");
+  //printArray(&list[0], size);
   start = clock();
   radixSort(&list[0], size);
   end = clock();
   cpu_timeu = ((double) (end - start)) / CLOCKS_PER_SEC;
   fprintf(stdout,"%f ", cpu_timeu);
-  printArray(&list[0], size);
+
+  //printArray(&list[0], size);
   printf("\n");
+  start = clock();
+  qsort(check, size, sizeof(int), cmpfunc);
+  end = clock();
+  cpu_timeu = ((double) (end - start)) / CLOCKS_PER_SEC;
+  fprintf(stdout,"qsort time = %f ", cpu_timeu);
   
+  for(i = 0; i< size; i++)
+    {
+      if (check[i] != list[i])
+	fprintf(stdout,"you fucked up son: %d, %d \n", check[i], list[i]);
+     
+    }
   return 0;
 }
